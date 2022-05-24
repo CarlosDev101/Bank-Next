fe477603d392c596ce7c79b0ab781eddf7351141
Carlos Eduardo <80906720+CarlosDev101@users.noreply.github.com>
2022-05-24 10:33:30 -0300

    Update Ligacao.java

--- src/br/com/projetonext/controller/Ligacao.java
+++ src/br/com/projetonext/controller/Ligacao.java
@@ -1,14 +1,19 @@
 package br.com.projetonext.controller;
 
+import br.com.projetonext.view.Application;
+
 public class Ligacao {
 	public String Ligar(String NomeUsuario, String CpfUsuario, String EmailUsuario, String SenhaUsuario) {
 
 		ClienteController ClienteControl = new ClienteController();
 		ContaController ContaControl = new ContaController();
-
-		ClienteControl.cadastarCliente(NomeUsuario, CpfUsuario, EmailUsuario, SenhaUsuario);
-		ContaControl.CriarContaControl();
-
+		
+		if(ClienteControl.cadastarCliente(NomeUsuario, CpfUsuario, EmailUsuario, SenhaUsuario) == true) {
+			ContaControl.CriarContaControl();
+		} else {
+			Application.Next();
+		}
+		
 		System.out.print("Conta criada!");
 		return "Ligacao concluida";
 
